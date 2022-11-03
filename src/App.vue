2fa2c5076030298fe66d195137b485b320663d8a
Jose A. Garcia Villanueva <jagarcia@grupomavi.com>
2022-11-03 02:29:00 +0100

    primer commit de la seccion 4 del curso de vue js

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ButtonCounter from './Components/ButtonCounter.vue'
+</script>
+<template>
+  <div class="container">
+    <h1>APP</h1>
+    <!--<button @click="increment">{{ counter }}</button>-->
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <button-counter />
+  </div>
+</template>
+
+<style>
+
+</style>
